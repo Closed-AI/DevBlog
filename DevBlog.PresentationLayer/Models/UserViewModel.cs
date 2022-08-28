c73d35ac4f258a8bc377325f45d92864b435d0ce
ARS <closedAI@mail.ru>
2022-08-28 22:01:44 +0300

    add user entity in Prezentation Layer

---
+++ DevBlog.PresentationLayer/Models/UserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DevBlog.PresentationLayer.Models
+{
+    public class UserViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string SecondName { get; set; }
+
+        public string ProfileImagePath { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+}
